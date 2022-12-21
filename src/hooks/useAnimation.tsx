b75cb6aace80f6fb9ed7d0ecbeb5561882ab4e62
crossbrz8 <vinicius.gomes@vortexz.com.br>
2022-12-21 00:37:02 -0300

    A animation hook was created

---
+++ src/hooks/useAnimation.tsx
@@ -0,0 +1,41 @@
+import { 
+    Easing, 
+    Extrapolate, 
+    interpolate, 
+    useAnimatedStyle, 
+    useDerivedValue, 
+    useSharedValue, 
+    withTiming
+} from "react-native-reanimated";
+
+interface AnimationButton {
+    valueScale?: number;
+}
+
+export const useAnimattion = (scaleTo?: AnimationButton) => {
+    const pressed = useSharedValue(false);
+
+    const value = scaleTo?.valueScale || 1.3;
+
+    const TimeConfigurations = {duration: 50, easing: Easing.linear};
+    const progress = useDerivedValue(() => {
+        return pressed.value
+          ? withTiming(1, TimeConfigurations)
+          : withTiming(0, TimeConfigurations);
+    });
+
+    const animatedStyle = useAnimatedStyle(() => {
+        const scale = interpolate(
+          progress.value,
+          [0, 1],
+          [1, value],
+          Extrapolate.CLAMP,
+        );
+    
+        return {
+          transform: [{scale}],
+        };
+    });
+
+    return {pressed, animatedStyle};
+}
