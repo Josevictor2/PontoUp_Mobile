64f43e56b3e7af32ed20fc7303d51412787de54d
crossbrz8 <vinicius.gomes@vortexz.com.br>
2022-12-23 00:17:43 -0300

    new context (useModal) was created

---
+++ src/contexts/Modal.tsx
@@ -0,0 +1,28 @@
+import { createContext, FC, ReactNode, useState } from "react";
+
+export type ModalContext = {
+    showModal: boolean;
+    setShowModal: (showModal: boolean) => void;
+    title: string;
+    setTitle: (title: string) => void;
+    modalStatus: number;
+    setModalStatus: (modalStatus: number) => void;
+}
+
+export const ModalContext = createContext({} as ModalContext);
+
+type ModalContextProviderProps = {
+    children: ReactNode;
+}
+
+export const ModalContextProvider: FC<ModalContextProviderProps> = ({ children }) => {
+    const [showModal, setShowModal] = useState<boolean>(false);
+    const [title, setTitle] = useState<string>("");
+    const [modalStatus, setModalStatus] = useState<number>(0);
+
+    return (
+        <ModalContext.Provider value={{showModal, setShowModal, title, setTitle, modalStatus, setModalStatus}}>
+            {children}
+        </ModalContext.Provider>
+    );
+}
