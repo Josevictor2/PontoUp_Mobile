0662401f4e4ca98639babce3799d3d505c61e9a5
crossbrz8 <vinicius.gomes@vortexz.com.br>
2022-12-24 02:20:11 -0300

    App was formatted with Prettier

--- App.tsx
+++ App.tsx
@@ -1,22 +1,22 @@
-import 'react-native-gesture-handler';
-import React from 'react';
+import "react-native-gesture-handler";
+import React from "react";
 import { NativeBaseProvider } from "native-base";
-import useCachedResources from './src/hooks/useCachedResources';
-import { StatusBar } from 'react-native';
-import { theme } from './src/theme';
-import { Router } from './src/routes/Router';
-import { AuthProvider } from './src/contexts/Auth';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { LinearGradient } from 'expo-linear-gradient';
-import { ModalContextProvider } from './src/contexts/Modal';
+import useCachedResources from "./src/hooks/useCachedResources";
+import { StatusBar } from "react-native";
+import { theme } from "./src/theme";
+import { Router } from "./src/routes/Router";
+import { AuthProvider } from "./src/contexts/Auth";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
+import { ModalContextProvider } from "./src/contexts/Modal";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
 
   const config = {
     dependencies: {
-      'linear-gradient': LinearGradient
-    }
+      "linear-gradient": LinearGradient,
+    },
   };
 
   if (!isLoadingComplete) {
