9310667cb99e2671c350813e95c937505cffd488
crossbrz8 <vinicius.gomes@vortexz.com.br>
2022-12-24 02:22:02 -0300

    Path aliases created

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,27 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".jsx", ".js", ".ts", ".tsx"],
+          alias: {
+            "@types": "./src/types",
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            contexts: "./src/contexts",
+            "@hooks": "./src/hooks",
+            "@routes": "./src/routes",
+            "@screens": "./src/screens",
+            "@services": "./src/services",
+            "@theme": "./src/theme",
+          },
+        },
+      ],
+    ],
   };
 };
