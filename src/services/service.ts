100492aae84044fa39f241a8f80bb93f8ee110df
crossbrz8 <vinicius.gomes@vortexz.com.br>
2022-12-24 02:30:29 -0300

    error: undefined value has been fixed

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,15 +1,16 @@
-import {User} from '../contexts/Auth';
+import { User } from "../contexts/Auth";
 
 const signIn = (password: string): Promise<User> => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (password === '123456') {
+      if (password === "123456") {
         resolve({
           token: JWTTokenMock,
-          name: 'Lucas Garcez',
+          name: "Lucas Garcez",
+          device: "mobile 1",
         });
       } else {
-        reject(new Error('credenciais incorretas'));
+        reject(new Error("credenciais incorretas"));
       }
     }, 1000);
   });
@@ -20,4 +21,4 @@ export const authService = {
 };
 
 const JWTTokenMock =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikx1Y2FzIEdhcmNleiIsImlhdCI6MTUxNjIzOTAyMn0.oK5FZPULfF-nfZmiumDGiufxf10Fe2KiGe9G5Njoa64';
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikx1Y2FzIEdhcmNleiIsImlhdCI6MTUxNjIzOTAyMn0.oK5FZPULfF-nfZmiumDGiufxf10Fe2KiGe9G5Njoa64";
