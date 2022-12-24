df257b14db714cca11156da4a474ea1f82ae9da3
crossbrz8 <vinicius.gomes@vortexz.com.br>
2022-12-24 02:32:27 -0300

    error: children as prop has been fixed

--- src/components/RegisterPoints/index.tsx
+++ src/components/RegisterPoints/index.tsx
@@ -1,62 +1,72 @@
 import { VStack } from "native-base";
 import { Button } from "../../components/Button";
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 import { useModal } from "../../hooks/useModal";
 
 export const RegisterButtons = () => {
-    const {setTitle, setShowModal, setModalStatus} = useModal();
-    
-    return (
-        <VStack mt="8px" p="20px" space={hp(1.2)} bg="secondary.50" borderRadius={8}>
-            <Button 
-            children="Iniciar expediente" 
-            h={hp(6.2)} 
-            bg="#30663C26" 
-            color="text.300"
-            onPress={() => {
-                setTitle("Iniciar expediente");
-                setShowModal(true);
-                setModalStatus(0);
-                }}
-            />
-            <Button 
-            children="Pausa" 
-            h={hp(6.2)} 
-            bg="#3FA4EE26" 
-            bgPressed="info.500" 
-            color="#2D9DEF"
-            onPress={() => {
-                setTitle("Registrar intervalo");
-                setShowModal(true);
-                setModalStatus(1);
-                }}
-            />
+  const { setTitle, setShowModal, setModalStatus } = useModal();
 
-            <Button 
-            children="Retorno" 
-            h={hp(6.2)} 
-            bg="#DF992F26" 
-            bgPressed="yellow.600" 
-            color="#DF992F"
-            onPress={() => {
-                setTitle("Registrar intervalo");
-                setShowModal(true);
-                setModalStatus(2);
-                }}
-            />
+  return (
+    <VStack
+      mt="8px"
+      p="20px"
+      space={hp(1.2)}
+      bg="secondary.50"
+      borderRadius={8}
+    >
+      <Button
+        h={hp(6.2)}
+        bg="#30663C26"
+        color="text.300"
+        onPress={() => {
+          setTitle("Iniciar expediente");
+          setShowModal(true);
+          setModalStatus(0);
+        }}
+      >
+        Iniciar expediente
+      </Button>
+      <Button
+        h={hp(6.2)}
+        bg="#3FA4EE26"
+        bgPressed="info.500"
+        color="#2D9DEF"
+        onPress={() => {
+          setTitle("Registrar intervalo");
+          setShowModal(true);
+          setModalStatus(1);
+        }}
+      >
+        Pausa
+      </Button>
 
-            <Button 
-            children="Finalizar expediente" 
-            h={hp(6.2)} 
-            bgPressed="red.500" 
-            bg="#D95D4226" 
-            color="#D95D42"
-            onPress={() => {
-                setTitle("Finalizar expediente");
-                setShowModal(true);
-                setModalStatus(3);
-                }}
-            />
-        </VStack>
-    )
-}
+      <Button
+        h={hp(6.2)}
+        bg="#DF992F26"
+        bgPressed="yellow.600"
+        color="#DF992F"
+        onPress={() => {
+          setTitle("Registrar intervalo");
+          setShowModal(true);
+          setModalStatus(2);
+        }}
+      >
+        Retorno
+      </Button>
+
+      <Button
+        h={hp(6.2)}
+        bgPressed="red.500"
+        bg="#D95D4226"
+        color="#D95D42"
+        onPress={() => {
+          setTitle("Finalizar expediente");
+          setShowModal(true);
+          setModalStatus(3);
+        }}
+      >
+        Finalizar expediente
+      </Button>
+    </VStack>
+  );
+};
