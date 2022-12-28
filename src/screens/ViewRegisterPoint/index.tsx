7043c5fd277dcb0dcac9076589c64c42693ff401
crossbrz8 <vinicius.gomes@vortexz.com.br>
2022-12-27 23:52:50 -0300

    feat: created nre page view

--- src/screens/ViewRegisterPoint/index.tsx
+++ src/screens/ViewRegisterPoint/index.tsx
@@ -1,14 +1,38 @@
-import { VStack, Text } from 'native-base';
+import { Button, VStack } from 'native-base';
 
-import { Button } from '@components/Button';
+import { Header } from '@components/Header';
 import { useAuth } from '@hooks/useAuth';
+//import { RenderItem } from '@components/RenderItem';
+
+const data = [
+  {
+    id: '1',
+    title:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget aliquam ultricies, nunc nisl aliquam nunc, eget aliquam nunc nisl eget nunc.',
+  },
+  {
+    id: '2',
+    title: 'Justification 2',
+  },
+  {
+    id: '3',
+    title: 'Justification 3',
+  },
+  {
+    id: '4',
+    title: 'Justification 4',
+  },
+];
 
 export const ViewRegisterScreen = () => {
   const { signOut } = useAuth();
   return (
-    <VStack flex={1} justifyContent="center" alignItems="center" safeArea>
-      <Text>Justification</Text>
-      <Button onPress={signOut}>Sair</Button>
+    <VStack h="100%" flex={1} safeArea>
+      <VStack bg="white" flex={1}>
+        <Header />
+        <Button onPress={() => signOut()}>sair</Button>
+      </VStack>
+      <VStack bg="blue.300" flex={5} />
     </VStack>
   );
 };
