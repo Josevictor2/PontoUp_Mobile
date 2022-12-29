e26196b58dda438f1b073b980dd5562cc2152f70
crossbrz8 <vinicius.gomes@vortexz.com.br>
2022-12-29 00:46:56 -0300

    Loading using Moti animation has been created

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
-import { Box, Spinner } from 'native-base';
+import { Image, Stack } from 'native-base';
+import { MotiView } from 'moti';
+import logo from '@assets/images/Logo.png';
+
+type sizeType = {
+  size: number;
+};
+
+const LoadingAnimated = ({ size }: sizeType) => {
+  return (
+    <MotiView
+      from={{
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        borderWidth: 0,
+        shadowOpacity: 0.5,
+      }}
+      animate={{
+        width: size + 20,
+        height: size + 20,
+        borderRadius: (size + 20) / 2,
+        borderWidth: size / 10,
+        shadowOpacity: 1,
+      }}
+      transition={{
+        type: 'timing',
+        duration: 1000,
+        loop: true,
+      }}
+      style={{
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        borderWidth: size / 10,
+        borderColor: '#30663C',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+      }}
+    />
+  );
+};
 
 export const LoadingFonts = () => {
   return (
-    <Box
+    <Stack
       flex={1}
       justifyContent="center"
       alignItems="center"
@@ -11,7 +54,16 @@ export const LoadingFonts = () => {
       w="100%"
       h="100%"
     >
-      <Spinner size="lg" color="#30663C" />
-    </Box>
+      <LoadingAnimated size={150} />
+      <Image
+        position="absolute"
+        zIndex={1}
+        width="120px"
+        height="60px"
+        resizeMode="contain"
+        source={logo}
+        alt="PontoUp"
+      />
+    </Stack>
   );
 };
