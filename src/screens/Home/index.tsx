f5e903f975152904b26950fedc010a3542ac1692
crossbrz8 <vinicius.gomes@vortexz.com.br>
2022-12-30 00:56:20 -0300

    flex box applied on screen home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,21 @@
+import { FormJustify } from '@components/ActionSheet';
+import { Header } from '@components/Header';
+import { IconButton } from '@components/IconButton';
+import { ModalComponent } from '@components/Modal';
+import { RegisterButtons } from '@components/RegisterPoints';
+import { Feather, MaterialCommunityIcons } from '@expo/vector-icons';
+import { useMotiScale } from '@hooks/useMotiScale';
 import { useNavigation } from '@react-navigation/native';
-
+import { useFontSize } from '@theme/responsiveFontSize';
+import { MotiView } from 'moti';
 import {
+  HStack,
+  ScrollView,
   VStack,
   Text,
-  HStack,
+  Box,
   Icon,
-  ScrollView,
   useDisclose,
-  StatusBar,
 } from 'native-base';
 
 import {
@@ -15,124 +23,98 @@ import {
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 
-import { Feather, MaterialCommunityIcons } from '@expo/vector-icons';
-import { IconButton } from '@components/IconButton';
-import { FormJustify } from '@components/ActionSheet';
-import { ModalComponent } from '@components/Modal';
-import { RegisterButtons } from '@components/RegisterPoints';
-
-import { Header } from '@components/Header';
-import { useFontSize } from '@theme/responsiveFontSize';
-
-import { MotiView } from 'moti';
-import { useMotiScale } from '@hooks/useMotiScale';
-
 export const HomeScreen = () => {
   const { onOpen, isOpen, onClose } = useDisclose();
   const navigation = useNavigation();
   const { FontSize } = useFontSize();
 
   const { handleToogle, toogleAnimation } = useMotiScale({ scale: 1.1 });
-
   const { handleToogle: handleToogle2, toogleAnimation: toogleAnimation2 } =
     useMotiScale({ scale: 1.1 });
-
   return (
-    <ScrollView bg="gray.50" flex={1}>
-      <StatusBar
-        barStyle={'dark-content'}
-        translucent
-        backgroundColor="white"
-      />
-      <VStack width="100%" safeArea>
-        <VStack bg="white">
-          <Header />
-          <HStack
-            mt={hp(3.6)}
-            mb={hp(2.1)}
-            px={wp('6.1%')}
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <VStack space={1}>
-              <Text
-                fontFamily="body"
-                fontWeight="300"
-                fontSize={FontSize(14)}
-                color="secondary.500"
-                lineHeight={hp(2.9)}
-              >
-                Olá,{' '}
-                <Text fontWeight="400" bold>
-                  Victor Gomes
-                </Text>
-              </Text>
-              <Text
-                fontFamily="body"
-                fontWeight="300"
-                color="gray.300"
-                fontSize="xs"
-                lineHeight={hp(2.2)}
-              >
-                Matrícula:{' '}
-                <Text bold fontWeight="400" color="secondary.500">
-                  123456
-                </Text>
-              </Text>
-            </VStack>
-            <VStack space={1}>
-              <Text
-                fontFamily="body"
-                fontWeight="400"
-                color="gray.300"
-                fontSize="2xs"
-                lineHeight={hp(1.8)}
-              >
-                Horas trabalhadas
-              </Text>
-              <Text
-                fontFamily="body"
-                fontWeight="400"
-                fontSize="xs"
-                color="secondary.400"
-                lineHeight={hp(2.1)}
-              >
-                00:00
-              </Text>
-            </VStack>
-          </HStack>
-        </VStack>
-
-        <VStack px={wp(6.4)}>
-          <HStack
-            alignItems="center"
-            mt={hp(4.2)}
-            width="100%"
-            justifyContent="space-between"
-          >
+    <VStack flex={1} safeArea>
+      <VStack bg="white" flex={1}>
+        <Header />
+        <HStack
+          px={wp('6.1%')}
+          flex={1}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <VStack space={1}>
             <Text
               fontFamily="body"
               fontWeight="300"
-              fontSize="xl"
-              lineHeight={hp(3.6)}
-              color="secondary.400"
+              fontSize={FontSize(14)}
+              color="secondary.500"
+              lineHeight={hp(2.9)}
             >
-              Registrar Ponto
+              Olá,{' '}
+              <Text fontWeight="400" bold>
+                Victor Gomes
+              </Text>
+            </Text>
+            <Text
+              fontFamily="body"
+              fontWeight="300"
+              color="gray.300"
+              fontSize="xs"
+              lineHeight={hp(2.2)}
+            >
+              Matrícula:{' '}
+              <Text bold fontWeight="400" color="secondary.500">
+                123456
+              </Text>
+            </Text>
+          </VStack>
+          <Box flexGrow={1} />
+          <VStack space={1}>
+            <Text
+              fontFamily="body"
+              fontWeight="400"
+              color="gray.300"
+              fontSize={FontSize(10)}
+              lineHeight={hp(1.8)}
+            >
+              Horas trabalhadas
             </Text>
             <Text
               fontFamily="body"
               fontWeight="400"
-              fontSize="xs"
-              lineHeight={hp(2.2)}
-              color="gray.400"
+              fontSize={FontSize(12)}
+              color="secondary.400"
+              lineHeight={hp(2.1)}
             >
-              12/11/2022 às 21:07 min
+              00:00
             </Text>
-          </HStack>
-
+          </VStack>
+        </HStack>
+      </VStack>
+      <VStack px={wp(6.4)} bg="gray.50" w="100%" flex={5}>
+        <HStack mt={hp(4.2)} justifyItems="center" alignItems="center">
+          <Text
+            fontFamily="body"
+            fontWeight="300"
+            fontSize="xl"
+            lineHeight={hp(3.6)}
+            color="secondary.400"
+          >
+            Registrar Ponto
+          </Text>
+          <Box flexGrow={1} />
+          <Text
+            fontFamily="body"
+            fontWeight="400"
+            fontSize="xs"
+            lineHeight={hp(2.2)}
+            color="gray.400"
+          >
+            12/11/2022 às 21:07 min
+          </Text>
+        </HStack>
+        <ScrollView>
           <RegisterButtons />
-
-          <VStack mt={hp(2.9)} space={hp(1.2)}>
+          <VStack mt={hp(1.9)}>
             <MotiView state={toogleAnimation}>
               <IconButton
                 onPressIn={handleToogle}
@@ -150,13 +132,13 @@ export const HomeScreen = () => {
                   mb={1}
                   fontFamily="body"
                   fontWeight="400"
-                  fontSize="md"
+                  fontSize={FontSize(14)}
                 >
                   Visualizar Frequência
                 </Text>
+                <Box flexGrow={1} />
                 <Icon
                   as={Feather}
-                  ml="auto"
                   name="chevron-right"
                   size="sm"
                   color="gray.100"
@@ -169,7 +151,6 @@ export const HomeScreen = () => {
                 onPressOut={handleToogle2}
                 height={hp(8.4)}
                 onPress={onOpen}
-                mb="10px"
               >
                 <Icon
                   as={<MaterialCommunityIcons name="calendar-clock-outline" />}
@@ -183,13 +164,13 @@ export const HomeScreen = () => {
                   mb={1}
                   fontFamily="body"
                   fontWeight="400"
-                  fontSize="md"
+                  fontSize={FontSize(14)}
                 >
                   Justificar falta
                 </Text>
+                <Box flexGrow={1} />
                 <Icon
                   as={Feather}
-                  ml="auto"
                   name="chevron-right"
                   size="sm"
                   color="gray.100"
@@ -197,10 +178,10 @@ export const HomeScreen = () => {
               </IconButton>
             </MotiView>
           </VStack>
-          <FormJustify isOpen={isOpen} onClose={onClose} />
-        </VStack>
+        </ScrollView>
+        <FormJustify isOpen={isOpen} onClose={onClose} />
       </VStack>
       <ModalComponent />
-    </ScrollView>
+    </VStack>
   );
 };
