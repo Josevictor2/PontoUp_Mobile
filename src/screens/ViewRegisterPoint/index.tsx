59847499277dc75c600da71c07e0ed860690b053
crossbrz8 <vinicius.gomes@vortexz.com.br>
2022-12-31 00:57:41 -0300

    added react memo in component and a new component RenderList

--- src/screens/ViewRegisterPoint/index.tsx
+++ src/screens/ViewRegisterPoint/index.tsx
@@ -1,7 +1,10 @@
-import { HStack, VStack, Text, Box, Pressable } from 'native-base';
+import { HStack, VStack, Text, Box, Pressable, FlatList } from 'native-base';
 import { format } from 'date-fns';
 
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 import { Header } from '@components/Header';
 import { useFontSize } from '@theme/responsiveFontSize';
@@ -10,7 +13,30 @@ import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 import { useSelectDate } from '@components/helpers/useSelectDate';
 import { pt } from 'date-fns/locale';
-//import { RenderItem } from '@components/RenderItem';
+import { ItemRender } from '@components/ItemRender';
+
+const data = [
+  {
+    id: '1',
+    title: 'First Item',
+    name: 'João',
+    date: '2021-07-01',
+  },
+  {
+    id: '2',
+    title: 'Second Item',
+    name: 'João',
+    date: '2021-07-02',
+  },
+  {
+    id: '3',
+    title: 'Third Item',
+    name: 'João',
+    date: '2021-07-03',
+  },
+];
+
+const ItemSeparator = () => <Box w="100%" h={2} />;
 
 export const ViewRegisterScreen = () => {
   const navigation = useNavigation();
@@ -84,7 +110,21 @@ export const ViewRegisterScreen = () => {
             <Feather name="chevron-right" size={24} />
           </Pressable>
         </HStack>
-        <VStack flex={9} bg="white" w="100%" />
+        <VStack flex={9} bg="white" w="100%">
+          <FlatList
+            mt={hp('1.6%')}
+            ItemSeparatorComponent={ItemSeparator}
+            data={data}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <ItemRender
+                date={item.date}
+                name={item.name}
+                title={item.title}
+              />
+            )}
+          />
+        </VStack>
       </VStack>
     </VStack>
   );
